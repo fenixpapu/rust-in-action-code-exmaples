b3565cc245e5455e536b858d385462956515eb08
papu <papu@papus-MacBook-Pro.local>
2022-12-20 01:03:30 +0700

    add c1-penguin to c1-hello2

--- c1-hello2/src/main.rs
+++ c1-hello2/src/main.rs
@@ -1,3 +1,35 @@
+fn main() {
+    let penguin_data = "\
+    common name, length (cm)
+    Little penguin, 33
+    Yellow-eyed penguin, 65
+    Fiordland penguin, 60
+    Invalid,data
+    ";
+
+    let records = penguin_data.lines();
+
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
+
+
+
+        if cfg!(debug_asseration) {
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        if let Ok(length) = fields[1].parse::<f32>() {
+            println!("{}, {} cm", name, length);
+        }
+    }
+    greet_world();
+}
+
 fn greet_world() {
     println!("Hello, world!");
     let southern_germany = "Grüß Gott!";
@@ -6,8 +38,4 @@ fn greet_world() {
     for region in regions.iter() {
         println!("{}", &region);
     }
-}
-
-fn main() {
-    greet_world();
 }
